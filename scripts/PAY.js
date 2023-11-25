cef11db8ea97eafb4d6f467c0cc9903039be67a7
lks <lisheng02091@gmail.com>
2023-11-25 22:21:39 +0000

    pagamento feito

--- scripts/PAY.js
+++ scripts/PAY.js
@@ -22,7 +22,7 @@
 
     function formatarValidade() {
         let validade = document.getElementById('validade').value;
-        
+
         validade = validade.replace(/\D/g, ''); // Remove caracteres não numéricos
         if (validade.length > 2) {
             validade = validade.substring(0, 2) + '/' + validade.substring(2);
@@ -30,3 +30,30 @@
         document.getElementById('validade').value = validade;
     }
 
+
+    function finalizarCompra() {
+        var numeroCartao = document.getElementById('numeroCartao').value;
+        var nomeUser = document.getElementById('nomeUser').value;
+        var validade = document.getElementById('validade').value;
+        var CVV = document.getElementById('CVV').value;
+        var numeroTelemovel = document.getElementById('numeroTelemovel').value;
+    
+        if ((numeroCartao && nomeUser && validade && CVV) || numeroTelemovel) {
+            document.getElementById('finalizarCompraBtn').disabled = true;
+            document.getElementById('verificacaoAnimacao').style.display = 'inline-block';
+    
+            setTimeout(function() {
+                document.getElementById('verificacaoAnimacao').style.display = 'none';
+                document.getElementById('modalAviso').style.display = 'flex';
+                document.getElementById('finalizarCompraBtn').disabled = false;
+            }, 2000);
+        } else {
+            alert('Preencha todos os campos de cartão ou MB WAY.');
+        }
+    }
+    
+    function fecharModal() {
+        document.getElementById('modalAviso').style.display = 'none';
+        document.getElementById('finalizarCompraBtn').disabled = false; 
+    }
+    
