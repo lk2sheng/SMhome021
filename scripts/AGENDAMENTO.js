8c367c81e8b4a7d888191a40a4047149117fcdbe
BotBlazing <guisecretkey@gmail.com>
2023-11-29 01:02:00 +0000

    Add files via upload

--- scripts/AGENDAMENTO.js
+++ scripts/AGENDAMENTO.js
@@ -29,6 +29,9 @@ function createScheduleBox(scheduleItem, index) {
             schedule.splice(itemIndex, 1);
             localStorage.setItem('Schedule', JSON.stringify(schedule));
             scheduleContainer.removeChild(scheduleBox);
+            for (let i = 0; i < schedule.length; i++){
+                document.getElementsByClassName("delete-icon")[i].setAttribute('data-index', i);
+            }
         });
 
         scheduleDetails.innerHTML = `
@@ -66,6 +69,9 @@ function createScheduleBox(scheduleItem, index) {
             schedule.splice(itemIndex, 1);
             localStorage.setItem('Schedule', JSON.stringify(schedule));
             scheduleContainer.removeChild(scheduleBox);
+            for (let i = 0; i < schedule.length; i++){
+                document.getElementsByClassName("delete-icon")[i].setAttribute('data-index', i);
+            }
         });
 
         scheduleDetails.innerHTML = `
@@ -102,6 +108,9 @@ function createScheduleBox(scheduleItem, index) {
             schedule.splice(itemIndex, 1);
             localStorage.setItem('Schedule', JSON.stringify(schedule));
             scheduleContainer.removeChild(scheduleBox);
+            for (let i = 0; i < schedule.length; i++){
+                document.getElementsByClassName("delete-icon")[i].setAttribute('data-index', i);
+            }
         });
 
         scheduleDetails.innerHTML = `
@@ -139,6 +148,9 @@ function createScheduleBox(scheduleItem, index) {
             schedule.splice(itemIndex, 1);
             localStorage.setItem('Schedule', JSON.stringify(schedule));
             scheduleContainer.removeChild(scheduleBox);
+            for (let i = 0; i < schedule.length; i++){
+                document.getElementsByClassName("delete-icon")[i].setAttribute('data-index', i);
+            }
         });
 
         scheduleDetails.innerHTML = `
@@ -163,7 +175,78 @@ function createScheduleBox(scheduleItem, index) {
         scheduleDetails.appendChild(deleteIcon);
         scheduleBox.appendChild(scheduleDetails);
     }
-    
+    if (scheduleItem.name == "ASPIRADOR") {
+        const scheduleDetails = document.createElement('div');
+        scheduleDetails.classList.add('schedule-details');
+
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('fas', 'fa-trash-alt', 'delete-icon');
+        deleteIcon.setAttribute('data-index', index);
+        deleteIcon.addEventListener('click', function () {
+            const itemIndex = parseInt(this.getAttribute('data-index'));
+            schedule.splice(itemIndex, 1);
+            localStorage.setItem('Schedule', JSON.stringify(schedule));
+            scheduleContainer.removeChild(scheduleBox);
+            for (let i = 0; i < schedule.length; i++){
+                document.getElementsByClassName("delete-icon")[i].setAttribute('data-index', i);
+            }
+        });
+
+        scheduleDetails.innerHTML = `
+            <p><b> ASPIRADOR </b></p>
+            <p><b><i class="bi bi-calendar" style="font-size: 20px;"></i> ${scheduleItem.dayOfWeek}</b></p>
+            <p><b><i class="bi bi-clock" style="font-size: 20px;"></i> INÍCIO:${scheduleItem.starthour}:${scheduleItem.startminute} | FIM: ${scheduleItem.endhour}: ${scheduleItem.endminute}</b></p>
+        `;
+        
+        if (scheduleItem.connection) {
+            scheduleDetails.innerHTML += `
+                <p><b> <i class="bi bi-lightning-fill text-warning" style="font-size: 20px;"></i> ESTADO: LIGAR </b></p>
+                <p><b> <i class="bi bi-gear-wide-connected" style="font-size: 20px;"></i> MODO: ${scheduleItem.mode}</b> </p>
+            `;
+        } else {
+            scheduleDetails.innerHTML += `
+            <p><b> <i class="bi bi-lightning-fill text-warning" style="font-size: 20px;"></i> ESTADO: LIGAR </b></p>
+                <p><b> <i class="bi bi-gear-wide-connected" style="font-size: 20px;"></i> MODO: ${scheduleItem.mode}</b> </p>
+            `;
+        }
+
+        scheduleDetails.appendChild(deleteIcon);
+        scheduleBox.appendChild(scheduleDetails);
+    }
+    if (scheduleItem.name == "MÁQUINA DE CAFÉ") {
+        const scheduleDetails = document.createElement('div');
+        scheduleDetails.classList.add('schedule-details');
+
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('fas', 'fa-trash-alt', 'delete-icon');
+        deleteIcon.setAttribute('data-index', index);
+        deleteIcon.addEventListener('click', function () {
+            const itemIndex = parseInt(this.getAttribute('data-index'));
+            schedule.splice(itemIndex, 1);
+            localStorage.setItem('Schedule', JSON.stringify(schedule));
+            scheduleContainer.removeChild(scheduleBox);
+            for (let i = 0; i < schedule.length; i++){
+                document.getElementsByClassName("delete-icon")[i].setAttribute('data-index', i);
+            }
+        });
+
+        scheduleDetails.innerHTML = `
+            <p><b> MÁQUINA DE CAFÉ </b></p>
+            <p><b><i class="bi bi-calendar" style="font-size: 20px;"></i> ${scheduleItem.dayOfWeek}</b></p>
+            <p><b><i class="bi bi-clock" style="font-size: 20px;"></i> ${scheduleItem.hour}:${scheduleItem.minute}</b></p>
+        `;
+
+        if (scheduleItem.connection) {
+            scheduleDetails.innerHTML += `
+                <p><b> <i class="bi bi-lightning-fill text-warning" style="font-size: 20px;"></i> ESTADO: TIRAR </b></p>
+                <p><b> <i class="bi bi-thermometer-half text-info" style="font-size: 20px;"></i>TEMP. ÁGUA: ${scheduleItem.temperature}ºC </b></p>
+                <p><b> <i class="bi bi-gear-wide-connected" style="font-size: 20px;"></i> TIPO: ${scheduleItem.mode}</b> </p>
+            `;
+        }
+
+        scheduleDetails.appendChild(deleteIcon);
+        scheduleBox.appendChild(scheduleDetails);
+    }
     scheduleContainer.appendChild(scheduleBox);
 }
 
@@ -180,6 +263,14 @@ function Menu_Perfil(){
 
 document.addEventListener('DOMContentLoaded', function() {
     schedule = JSON.parse(localStorage.getItem('Schedule')) || [];
+    if (schedule.length == 0){
+        let container = document.getElementById('scheduleContainer');
+        let textBox = document.createElement("div");
+        textBox.classList.add('infoBox');
+
+        textBox.innerHTML = "Ainda não foram definidos agendamentos.";
+        container.appendChild(textBox);
+    }
 
     schedule.sort((a, b) => {
         const timeA = parseInt(a.hour) * 60 + parseInt(a.minute);
