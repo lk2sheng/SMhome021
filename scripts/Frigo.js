028b7be408993e23e5524b357b99e43f3181f3e0
BotBlazing <guisecretkey@gmail.com>
2023-12-01 22:48:15 +0000

    Add files via upload

--- scripts/Frigo.js
+++ scripts/Frigo.js
@@ -1,10 +1,33 @@
+"use strict";
+
+class Item {
+    constructor(name, expirationDate, image, n = 1) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+        this.image = image;
+        this.n = n;
+    }
+
+}
+
+
 let searchFrom = document.querySelector(".search-form");
+
+const stock = [new Item("Queijo", "2023-12-20", "../media/image-firgo-6.jpg", 1), 
+    new Item("Leite", "2023-11-3", "../media/image-firgo-2.jpg", 3), 
+new Item("Iogurte", "2023-12-25", "../media/image-firgo-13.jpg", 2)];
+
+const suggested = [new Item("Farinha", "none", "../media/farinha.jpg"),
+  new Item("Leite", "none", "../media/image-firgo-2.jpg"),
+  new Item("Ovos", "none", "../media/image-firgo-10.jpg"), 
+  new Item('Frango', "none", "../media/image-firgo-9.jpg")];
+
 
 document.querySelector("#search_icon").onclick = () =>{
     searchFrom.classList.toggle("active");
 }
 
-document.addEventListener("scroll", function() {
+function scrollHelper() {
     // Get the scroll position
     let scrollPosition = window.scrollY;
   
@@ -21,121 +44,21 @@ document.addEventListener("scroll", function() {
         movingBox.classList.remove("active");
     }
     
-  });
+};
 
-  var products = [
-    {
-        image: "../media/image-firgo-2.jpg",
-        name: "Cenoura",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-2.jpg",
-        name: "Leite Açores",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-3.jpg",
-        name: "Leite Lactea",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-4.jpg",
-        name: "Leite Lactea",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-5.jpg",
-        name: "Cogumelos",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-6.jpg",
-        name: "Queijo",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-7.jpg",
-        name: "Agua",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-8.jpg",
-        name: "Product 1",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-9.jpg",
-        name: "Frango",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-10.jpg",
-        name: "Peru",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-11.jpg",
-        name: "Ovos",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-12.jpg",
-        name: "Piemntos",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-13.jpg",
-        name: "Iogurte",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-14.jpg",
-        name: "Cerveja",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-15.jpg",
-        name: "Vinho",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-2.jpg",
-        name: "Product 1",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-2.jpg",
-        name: "Product 1",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    {
-        image: "../media/image-firgo-2.jpg",
-        name: "Product 1",
-        description: "Validade: \n 15/12/2023",
-        link: "#"
-    },
-    // Add more products as needed
-];
+function Menu_Perfil(){
+    const userMenu = document.getElementById('user-menu');
+    if (userMenu.style.display === 'block') {
+        userMenu.style.display = 'none';
+    } else {
+        userMenu.style.display = 'block';
+    }
+}
 
-// Function to create a product box
+function GoBack() {
+    window.history.back();
+}
+
 function createProductBox(product) {
     var box = document.createElement("div");
     box.className = "box";
@@ -146,63 +69,43 @@ function createProductBox(product) {
     var h3 = document.createElement("h3");
     h3.textContent = product.name;
 
-    var p = document.createElement("p");
-    console.log(product.description);
-    p.textContent = product.description;
-
-    // Append elements to the box
-    box.appendChild(img);
-    box.appendChild(h3);
-    box.appendChild(p);
-
-    return box;
-}
-
-// Function to create a product box
-function createProductBox_Inv(product) {
-    var box = document.createElement("div");
-    box.className = "box";
-
-    var img = document.createElement("img");
-    img.src = product.image;
-
-    var h3 = document.createElement("h3");
-    h3.textContent = product.name;
-
-    var p = document.createElement("p");
-    console.log(product.description);
-    p.textContent = product.description;
-
-
-    // Append elements to the box
-    box.appendChild(img);
-    box.appendChild(h3);
-    box.appendChild(p);
+    if (product.expirationDate != "none"){
+        var p = document.createElement("p");
+        p.textContent = "Consumir antes de: " + product.expirationDate;
+    }
     
 
+    // Append elements to the box
+    box.appendChild(img);
+    box.appendChild(h3);
+    if (product.expirationDate != "none"){
+        box.appendChild(p);
+    }
+
     return box;
 }
 
-// Function to shuffle an array randomly
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+
+    //return array.slice(Math.floor(Math.random() * (array.length-1)));
     return array;
 }
 
 // Function to get or generate the randomized products
 function getOrGenerateRandomizedProducts() {
-    // Check if the products are already stored in session storage
+    // Check if the products are already stored in local storage
     var storedProducts = sessionStorage.getItem("randomizedProducts");
 
     if (storedProducts) {
         // If products are stored, parse and return them
         return JSON.parse(storedProducts);
     } else {
-        // If no products are stored, shuffle the products, store them in session storage, and return
-        var shuffledProducts = shuffleArray(products.slice());
+        // If no products are stored, shuffle the products, store them, and return
+        var shuffledProducts = shuffleArray(suggested);
         sessionStorage.setItem("randomizedProducts", JSON.stringify(shuffledProducts));
         return shuffledProducts;
     }
@@ -211,13 +114,11 @@ function getOrGenerateRandomizedProducts() {
 // Get or generate the randomized products
 var randomizedProducts = getOrGenerateRandomizedProducts();
 
-// Adjust the ratio of products between products1 and products2
-// For example, let's generate 70% of the products for products1 and 30% for products2
 var ratio = 0.7;
 var products1Count = Math.ceil(randomizedProducts.length * ratio);
-var products1 = randomizedProducts.slice(0, products1Count);
-var products2 = randomizedProducts.slice(products1Count);
-
+// Split the randomized array into two
+var products1 = stock;
+var products2 = randomizedProducts;
 
 // Function to display products
 function displayProducts1(filteredProducts) {
@@ -230,7 +131,7 @@ function displayProducts1(filteredProducts) {
         container.appendChild(noResultsMessage);
     } else {
         filteredProducts.forEach(function(product) {
-            var box = createProductBox_Inv(product);
+            var box = createProductBox(product);
             container.appendChild(box);
         });
     }
@@ -252,22 +153,6 @@ function displayProducts2(filteredProducts) {
     }
 }
 
-
-document.getElementById("search-box").addEventListener("input", function() {
-    var searchTerm = this.value.toLowerCase();
-
-    var filteredProducts1 = products1.filter(function(product) {
-        return product.name.toLowerCase().startsWith(searchTerm);
-    });
-
-    var filteredProducts2 = products2.filter(function(product) {
-        return product.name.toLowerCase().startsWith(searchTerm);
-    });
-
-    displayProducts1(filteredProducts1);
-    displayProducts2(filteredProducts2);
-});
-
 function mostrarSugestoes() {
     
     // Disable the button
@@ -285,6 +170,7 @@ function mostrarSugestoes() {
     // Atualizar o texto do heading para "As Nossas Sugestões"
     document.querySelector(".heading").innerHTML = 'As Nossas <span>Sugestões</span>';
 }
+
 
 function mostrarFrigo() {
 
@@ -304,7 +190,42 @@ function mostrarFrigo() {
     document.querySelector(".heading").innerHTML = 'O Teu <span>Frigorífico</span>';
 }
 
-// Call the displayProducts function to show products on page load
-mostrarFrigo();
-displayProducts1(products1);
-displayProducts2(products2);
+
+function defineEventHandlersParaElementosHTML(){
+    const userIcon = document.getElementById('user-icon');
+    const itemInfoButton = document.getElementById('itemInfoButton');
+    const BackButton = document.getElementById('back-button');
+
+    document.addEventListener("scroll", scrollHelper);
+    userIcon.addEventListener("click", Menu_Perfil);
+    BackButton.addEventListener("click", GoBack);   
+
+    
+    document.getElementById("search-box").addEventListener("input", function() {
+    var searchTerm = this.value.toLowerCase();
+
+    var filteredProducts1 = products1.filter(function(product) {
+        return product.name.toLowerCase().startsWith(searchTerm);
+    });
+
+    var filteredProducts2 = products2.filter(function(product) {
+        return product.name.toLowerCase().startsWith(searchTerm);
+    });
+
+    displayProducts1(filteredProducts1);
+    displayProducts2(filteredProducts2);
+});
+}
+
+
+
+function principal(){
+    defineEventHandlersParaElementosHTML();
+
+    mostrarFrigo();
+    displayProducts1(products1);
+    displayProducts2(products2);
+  
+}
+
+window.addEventListener("load", principal);
